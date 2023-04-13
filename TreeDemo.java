9ea8d6015350f6b87cc3196f5dd9bb1bd85e26d1
Diego Rios <diegorios1199@hotmail.com>
2023-04-12 20:41:41 -0700

    Implemented the preOrderTraversal method

--- TreeDemo.java
+++ TreeDemo.java
@@ -39,6 +39,13 @@ class BinarySearchTree {
     */
    public void preOrderTraversal(Node root) {
       // implement me
+      if (root == null) {
+         return;
+      }
+
+      System.out.println(root.value + ", ");
+      preOrderTraversal(root.left);
+      preOrderTraversal(root.right);
    }
 
    /*
